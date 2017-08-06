9c3f106c729fd15d548e7160af8b589bc0291012
Joachim Metz <joachim.metz@gmail.com>
2017-08-06 12:11:28 +0200

    Worked on tests

---
+++ tests/pyfwsi_test_extension_block.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#
+# Python-bindings extension_block type test script
+#
+# Copyright (C) 2010-2017, Joachim Metz <joachim.metz@gmail.com>
+#
+# Refer to AUTHORS for acknowledgements.
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import os
+import sys
+import unittest
+
+import pyfwsi
+
+
+class ExtensionBlockTypeTests(unittest.TestCase):
+  """Tests the extension_block type."""
+
+
+if __name__ == "__main__":
+  argument_parser = argparse.ArgumentParser()
+
+  argument_parser.add_argument(
+      "source", nargs="?", action="store", metavar="PATH",
+      default=None, help="The path of the source file.")
+
+  options, unknown_options = argument_parser.parse_known_args()
+  unknown_options.insert(0, sys.argv[0])
+
+  setattr(unittest, "source", options.source)
+
+  unittest.main(argv=unknown_options, verbosity=2)
--- tests/test_library.sh
+++ tests/test_library.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 # Tests C library functions and types.
 #
-# Version: 20170115
+# Version: 20170722
 
 EXIT_SUCCESS=0;
 EXIT_FAILURE=1;
 EXIT_IGNORE=77;
 
-TEST_PREFIX=`dirname ${PWD}`;
-TEST_PREFIX=`basename ${TEST_PREFIX} | sed 's/^lib\([^-]*\).*$/\1/'`;
-
-TEST_PROFILE="lib${TEST_PREFIX}";
 LIBRARY_TESTS="cdburn_values compressed_folder_values control_panel_category_values control_panel_cpl_file_values delegate_values error extension_block extension_block_0xbeef0000_values extension_block_0xbeef0001_values extension_block_0xbeef0003_values extension_block_0xbeef0005_values extension_block_0xbeef0006_values extension_block_0xbeef0014_values extension_block_0xbeef0019_values extension_block_0xbeef0025_values file_entry_extension_values file_entry_values game_folder_values item item_list mtp_volume_values network_location_values root_folder_values support uri_values users_property_view_values volume_values";
 LIBRARY_TESTS_WITH_INPUT="";
 OPTION_SETS="";
 
-TEST_TOOL_DIRECTORY=".";
-INPUT_DIRECTORY="input";
 INPUT_GLOB="*";
 
 run_test()
@@ -24,7 +18,7 @@ run_test()
 	local TEST_NAME=$1;
 
 	local TEST_DESCRIPTION="Testing: ${TEST_NAME}";
-	local TEST_EXECUTABLE="${TEST_TOOL_DIRECTORY}/${TEST_PREFIX}_test_${TEST_NAME}";
+	local TEST_EXECUTABLE="./fwsi_test_${TEST_NAME}";
 
 	if ! test -x "${TEST_EXECUTABLE}";
 	then
@@ -43,14 +37,14 @@ run_test_with_input()
 	local TEST_NAME=$1;
 
 	local TEST_DESCRIPTION="Testing: ${TEST_NAME}";
-	local TEST_EXECUTABLE="${TEST_TOOL_DIRECTORY}/${TEST_PREFIX}_test_${TEST_NAME}";
+	local TEST_EXECUTABLE="./fwsi_test_${TEST_NAME}";
 
 	if ! test -x "${TEST_EXECUTABLE}";
 	then
 		TEST_EXECUTABLE="${TEST_EXECUTABLE}.exe";
 	fi
 
-	run_test_on_input_directory "${TEST_PROFILE}" "${TEST_DESCRIPTION}" "default" "${OPTION_SETS}" "${TEST_EXECUTABLE}" "${INPUT_DIRECTORY}" "${INPUT_GLOB}";
+	run_test_on_input_directory "libfwsi" "${TEST_DESCRIPTION}" "default" "${OPTION_SETS}" "${TEST_EXECUTABLE}" "input" "${INPUT_GLOB}";
 	local RESULT=$?;
 
 	return ${RESULT};
@@ -97,7 +91,7 @@ fi
 
 for TEST_NAME in ${LIBRARY_TESTS_WITH_INPUT};
 do
-	if test -d ${INPUT_DIRECTORY};
+	if test -d "input";
 	then
 		run_test_with_input "${TEST_NAME}";
 		RESULT=$?;
