bf1a2adfebe1ba4bc379e54d1aca80f41a7dbb6b
Joachim Metz <joachim.metz@gmail.com>
2018-01-18 07:17:26 +0100

    Applied updates

--- pyfwsi/pyfwsi_extension_block.c
+++ pyfwsi/pyfwsi_extension_block.c
@@ -1,7 +1,7 @@
 /*
  * Python object wrapper of libfwsi_extension_block_t
  *
- * Copyright (C) 2010-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
