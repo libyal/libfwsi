206910806387852a79918f1c6b523882a53ef031
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 16:22:38 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfwsi/.libs/libfwsi.1.dylib && test -f ./pyfwsi/.libs/pyfwsi.so
+if [ -f "${PWD}/libfwsi/.libs/libfwsi.1.dylib" ] && [ -f ./pyfwsi/.libs/pyfwsi.so ]
 then
-	install_name_tool -change /usr/local/lib/libfwsi.1.dylib ${PWD}/libfwsi/.libs/libfwsi.1.dylib ./pyfwsi/.libs/pyfwsi.so
+    install_name_tool -change /usr/local/lib/libfwsi.1.dylib "${PWD}/libfwsi/.libs/libfwsi.1.dylib" ./pyfwsi/.libs/pyfwsi.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
